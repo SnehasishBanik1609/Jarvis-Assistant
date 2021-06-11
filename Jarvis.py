a5bc5d06596b0f7a5b03a964aa5e8f57e6847b6c
SnehasishBanik1609 <rana2013.banik@gmail.com>
2021-06-11 17:58:07 +0530

    Add files via upload

---
+++ Jarvis.py
@@ -0,0 +1,281 @@
+import pyttsx3
+import wikipedia
+import speech_recognition as sr
+import webbrowser
+import datetime
+import os
+import sys
+import smtplib
+import psutil
+import pyjokes
+import pyautogui
+import psutil
+import pyjokes
+from sys import platform
+import os
+import getpass
+# from news import news, getNewsUrl
+# from diction import translate
+# from loc import weather
+# from youtube import youtube
+# from quote import tell_quote
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# print(voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def screenshot():
+    img = pyautogui.screenshot()
+    img.save('D://aptitude//screenshot.png')
+
+
+def cpu():
+    usage = str(psutil.cpu_percent())
+    speak("CPU is at"+usage)
+
+    battery = psutil.sensors_battery()
+    speak("battery is at")
+    speak(battery.percent)
+
+
+def joke():
+    for i in range(5):
+        speak(pyjokes.get_jokes()[i])
+
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.energy_threshold = 200
+        r.adjust_for_ambient_noise(source, duration=1.5)
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing..')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'User said: {query}\n')
+
+    except Exception as e:
+        # print(e)
+
+        print('Say that again please...')
+        return 'None'
+    return query
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning SIR")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon SIR")
+
+    else:
+        speak('Good Evening SIR')
+
+    # weather()
+    speak('I am JARVIS. Please tell me how can I help you SIR?')
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('email', 'password')
+    server.sendmail('email', to, content)
+    server.close()
+
+
+def cpu():
+    usage = str(psutil.cpu_percent())
+    speak("CPU is at"+usage)
+
+    battery = psutil.sensors_battery()
+    speak("battery is at")
+    speak(battery.percent)
+
+
+def joke():
+    for i in range(5):
+        speak(pyjokes.get_jokes()[i])
+
+
+def screenshot():
+    img = pyautogui.screenshot()
+    img.save('D://aptitude//screenshot.png')
+
+
+if __name__ == '__main__':
+
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia....')
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=2)
+            speak('According to Wikipedia')
+            print(results)
+            speak(results)
+
+        elif 'youtube downloader' in query:
+            exec(open('youtube_downloader.py').read())
+
+        elif 'voice' in query:
+            if 'female' in query:
+                engine.setProperty('voice', voices[1].id)
+            else:
+                engine.setProperty('voice', voices[0].id)
+            speak("Hello Sir, I have switched my voice. How is it?")
+        
+        if 'inspirational quote' in query:
+            tell_quote()
+        if 'jarvis are you there' in query:
+            speak("Yes Sir, at your service")
+
+        elif 'open youtube' in query:
+            webbrowser.open_new_tab('https://youtube.com')
+
+        elif 'cpu' in query:
+            cpu()
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'screenshot' in query:
+            speak("taking screenshot")
+            screenshot()
+
+        elif 'open google' in query:
+            webbrowser.open_new_tab('https://google.com')
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open_new_tab('https://stackoverflow.com')
+
+        elif 'play music' in query:
+             
+            speak('playing')
+            music_dir = "D:\\songs"
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        # elif 'search youtube' in query:
+        #     speak('What you want to search on Youtube?')
+        #     youtube(takeCommand())
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f'Sir, the time is {strTime}')
+
+        elif 'search' in query:
+            speak('What do you want to search for?')
+            search = takeCommand()
+            url = 'https://google.com/search?q=' + search
+            webbrowser.open_new_tab(
+                url)
+            speak('Here is What I found for' + search)
+
+        elif 'location' in query:
+            speak('What is the location?')
+            location = takeCommand()
+            url = 'https://google.nl/maps/place/' + location + '/&amp;'
+            webbrowser.open_new_tab(url)
+            speak('Here is the location ' + location)
+
+        elif 'your master' in query:
+            if platform == "win32" or "darwin":
+                speak('you are my master. you created me couple of days ago')
+            elif platform == "linux" or platform == "linux2":
+                name = getpass.getuser()
+                speak(name, 'is my master. He is running me right now')
+
+        elif 'your name' in query:
+            speak('My name is JARVIS')
+        elif 'stands for' in query:
+            speak('J.A.R.V.I.S stands for JUST A RATHER VERY INTELLIGENT SYSTEM')
+        elif 'open code' in query:
+            if platform == "win32":
+                os.startfile(
+                    "C:\\Users\\sansk\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+            elif platform == "linux" or platform == "linux2" or "darwin":
+                os.system('code .')
+
+        elif 'shutdown' in query:
+            if platform == "win32":
+                os.system('shutdown /p /f')
+            elif platform == "linux" or platform == "linux2" or "darwin":
+                os.system('poweroff')
+
+        elif 'cpu' in query:
+            cpu()
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'screenshot' in query:
+            speak("taking screenshot")
+            screenshot()
+
+        elif 'github' in query:
+            webbrowser.open_new_tab(
+                'https://github.com/')
+
+        elif 'remember that' in query:
+            speak("what should i remember sir")
+            rememberMessage = takeCommand()
+            speak("you said me to remember"+rememberMessage)
+            remember = open('data.txt', 'w')
+            remember.write(rememberMessage)
+            remember.close()
+
+        elif 'do you remember anything' in query:
+            remember = open('data.txt', 'r')
+            speak("you said me to remember that" + remember.read())
+
+        elif 'sleep' in query:
+            sys.exit()
+
+        # elif 'dictionary' in query:
+        #     speak('What you want to search in your intelligent dictionary?')
+        #     translate(takeCommand())
+
+        # elif 'news' in query:
+        #     speak('Ofcourse sir..')
+        #     speak_news()
+        #     speak('Would you like to read the while article?')
+        #     test = takeCommand()
+        #     if 'yes' in test:
+        #         speak('Ok Sir, Opening browser...')
+        #         webbrowser.open(getNewsUrl())
+        #         speak('You can now read the full news from this website.')
+        #     else:
+        #         speak('No Problem Sir')
+
+        elif 'voice' in query:
+            if 'female' in query:
+                engine.setProperty('voice', voices[0].id)
+            else:
+                engine.setProperty('voice', voices[1].id)
+            speak("Hello sir, I have switched my voice. How do you like it?")
+
+        elif 'email to raunak' in query:
+            try:
+                speak('What should I say?')
+                content = takeCommand()
+                to = 'email'
+                sendEmail(to, content)
+                speak('Email has been sent!')
+
+            except Exception as e:
+                speak('Sorry sir, but I am not able to send an email at the moment.')
